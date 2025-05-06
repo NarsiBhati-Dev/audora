a8a2edec6c7f60fa0bc537b25d058e626ecaa3ae
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-05-07 01:22:42 +0530

    fix: header

--- apps/audora-frontend/src/app/page.tsx
+++ apps/audora-frontend/src/app/page.tsx
@@ -1,15 +1,5 @@
+import HomePage from "@/components/landing-page/home";
+
 export default function Home() {
-  return (
-    <>
-      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
-        Audora
-      </main>
-      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
-        Audora
-      </main>
-      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
-        Audora
-      </main>
-    </>
-  );
+  return <HomePage />;
 }
---
+++ apps/audora-frontend/src/components/landing-page/home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
+        Audora
+      </main>
+      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
+        Audora
+      </main>
+      <main className="bg-black h-screen w-screen text-7xl font-extrabold text-white flex justify-center items-center">
+        Audora
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
