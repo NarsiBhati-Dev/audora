ee8bd4a0b7a1feca30e0ddcb4c99f04a77ac25cb
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-05-06 13:05:03 +0530

    feat: shared tailwind-css package

--- apps/audora-frontend/src/app/layout.tsx
+++ apps/audora-frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/globals.css";
+import "@audora/tailwind-css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +24,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {/* favicons */}
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicons/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="Audora" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
