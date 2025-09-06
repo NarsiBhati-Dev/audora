26c69e2b5cefe0021efc809adbd49f0f837ae5de
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-09-06 15:46:16 +0530

    remove recording pipeline

--- apps/audora-recorder-pipeline/src/config.ts
+++
@@ -1,5 +0,0 @@
-export const AWS_REGION = "ap-south-1";
-export const AWS_ACCESS_KEY_ID = "AKIA2222222222222222";
-export const AWS_SECRET_ACCESS_KEY = "22222222222222222222222222222222";
-export const AWS_S3_BUCKET_NAME = "audora-recordings";
-export const KAFKA_BROKERS = ["localhost:9092"];
